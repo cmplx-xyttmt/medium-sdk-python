f89736020a4cce2c7f1fbe15693dbd98033e40e2
cmplx-xyttmt <archimedesisaac@gmail.com>
2018-08-22 15:53:57 +0300

    Add test that checks whether an exception is raised when creating an invalid post.

--- tests/test_isaac.py
+++ tests/test_isaac.py
@@ -3,13 +3,49 @@ import unittest
 from urllib.parse import parse_qs
 import json
 
-from medium import Client
+from medium import Client, MediumError
 
 
 class TestIsaac(unittest.TestCase):
 
     def setUp(self):
         self.client = Client(access_token="isaac-token")
+
+    @responses.activate
+    def test_create_post_fails(self):
+        """Tests whether MediumError exception occurs if publish_status is not in the payload"""
+        def response_callback(payload):
+            data = {
+                "license": "all-rights-reserved",
+                "title": "Starships",
+                "url": "https://medium.com/@nicki/55050649c95",
+                "tags": ["stars", "ships", "pop"],
+                "authorId": "5303d74c64f66366f00cb9b2a94f3251bf5",
+                "publishStatus": "draft",
+                "id": "55050649c95",
+            }
+
+            if "publish_status" in payload:
+                return 200, data
+
+            return 400, {"error": ["Bad Request"]}
+
+        self._mock_endpoint(
+            "POST",
+            "/v1/users/5303d74c64f66366f00cb9b2a94f3251bf5/posts",
+            response_callback
+        )
+
+        with self.assertRaises(MediumError) as context:
+            self.client.create_post(
+                "5303d74c64f66366f00cb9b2a94f3251bf5",
+                "Starships",
+                "<p>Are meant to flyyyy</p>",
+                "html",
+                tags=["stars", "ships", "pop"]
+            )
+
+        self.assertEqual('API request failed', str(context.exception))
 
     def _mock_endpoint(self, method, path, callback, is_json=True):
         def wrapped_callback(req):
